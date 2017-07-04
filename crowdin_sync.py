2eb46252726114be6e00d5e7fb2439e8d084a45b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-07-04 12:10:26 -0300

    Refactor for AEX

--- crowdin_sync.py
+++ crowdin_sync.py
@@ -3,11 +3,12 @@
 # crowdin_sync.py
 #
 # Updates Crowdin source translations and pushes translations
-# directly to PureNexus Github.
+# directly to AospExtended Github.
 #
 # Copyright (C) 2014-2015 The CyanogenMod Project
 # This code has been modified. Portions copyright (C) 2016, The PAC-ROM Project
-#
+# This code has been modified. Portions copyright (C) 2017, AospExtended
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -75,7 +76,7 @@ def push_as_commit(base_path, path, name, branch, username):
 
     # Create commit; if it fails, probably empty so skipping
     try:
-        repo.git.commit(m='PureNexus Automatic translation import')
+        repo.git.commit(m='Automatic translation import')
     except:
         print('Failed to create commit for %s, probably empty: skipping'
               % name, file=sys.stderr)
@@ -83,7 +84,7 @@ def push_as_commit(base_path, path, name, branch, username):
 
     # Push commit
     try:
-        repo.git.push('git@github.com:PureNexusProject/%s' % (name),
+        repo.git.push('git@github.com:AospExtended/%s' % (name),
                       'HEAD:%s' % branch)
         print('Successfully pushed commit for %s' % name)
     except:
@@ -109,16 +110,16 @@ def find_xml(base_path):
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Synchronising PureNexus translations with Crowdin")
+        description="Synchronising translations with Crowdin")
     sync = parser.add_mutually_exclusive_group()
-    parser.add_argument('-u', '--username', help='Gerrit username',
+    parser.add_argument('-u', '--username', help='Git username',
                         required=True)
-    parser.add_argument('-b', '--branch', help='Pure branch',
+    parser.add_argument('-b', '--branch', help='Branch',
                         required=True)
     sync.add_argument('--no-upload', action='store_true',
-                      help='Only download PureNexus translations from Crowdin')
+                      help='Only download translations from Crowdin')
     sync.add_argument('--no-download', action='store_true',
-                      help='Only upload PureNexus source translations to Crowdin')
+                      help='Only upload source translations to Crowdin')
     return parser.parse_args()
 
 # ################################# PREPARE ################################## #
@@ -264,15 +265,15 @@ def main():
     args = parse_args()
     default_branch = args.branch
 
-    base_path = os.getenv('PURE_CROWDIN_BASE_PATH')
+    base_path = os.getenv('CROWDIN_BASE_PATH')
     if base_path is None:
         cwd = os.getcwd()
-        print('You have not set PURE_CROWDIN_BASE_PATH. Defaulting to %s' % cwd)
+        print('You have not set CROWDIN_BASE_PATH. Defaulting to %s' % cwd)
         base_path = cwd
     else:
         base_path = os.path.join(os.path.realpath(base_path))
     if not os.path.isdir(base_path):
-        print('PURE_CROWDIN_BASE_PATH + branch is not a real directory: c'
+        print('CROWDIN_BASE_PATH + branch is not a real directory: c'
               % base_path)
         sys.exit(1)
 
