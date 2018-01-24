83b9e19cf136f4418492b1c9020c54a87dae1e3d
Shubham Singh <coolsks94@gmail.com>
2018-01-25 04:41:47 +0530

    crowdin: Push to gerrit from now
    Change-Id: I7c848fc4248656444a6111e512807d90ceb80bc9
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- crowdin_sync.py
+++ crowdin_sync.py
@@ -3,7 +3,7 @@
 # crowdin_sync.py
 #
 # Updates Crowdin source translations and pushes translations
-# directly to Superior OS Github.
+# directly to Superior OS Gerrit.
 #
 # Copyright (C) 2014-2015 The CyanogenMod Project
 # This code has been modified. Portions copyright (C) 2016, The PAC-ROM Project
@@ -84,8 +84,8 @@ def push_as_commit(base_path, path, name, branch, username):
 
     # Push commit
     try:
-        repo.git.push('git@github.com:SuperiorOS/%s' % (name),
-                      'HEAD:%s' % branch)
+        repo.git.push('ssh://%s@gerrit.superioros.org:29418/%s' % (username, name),
+                      'HEAD:refs/for/%s%%topic=translation' % branch)
         print('Successfully pushed commit for %s' % name)
     except:
         print('Failed to push commit for %s' % name, file=sys.stderr)
@@ -191,7 +191,7 @@ def download_crowdin(base_path, branch, xml, username, no_download=False):
         for p in str(comm[0]).split("\n"):
             paths.append(p.replace('/%s' % branch, ''))
 
-    print('\nUploading translations to Github')
+    print('\nUploading translations to Gerrit')
     xml_android = load_xml(x='%s/manifest/crowdin.xml' % base_path)
     items = xml_android.getElementsByTagName('project')
     #items = [x for sub in xml for x in sub.getElementsByTagName('project')]
