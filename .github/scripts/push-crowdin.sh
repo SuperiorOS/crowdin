93c193c7bbfbf447c724d20644d653cbb5b732b0
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2022-12-30 07:21:33 +0000

    Initial workflow setup
    - Sync every Sunday at 12AM night
    
    To-Do: Make download work
    
    Change-Id: I55e5f34b96ba7f1a337fad2eee5685c3c756e817
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

---
+++ .github/scripts/push-crowdin.sh
@@ -0,0 +1,9 @@
+          mkdir superior
+          cd superior
+          repo init -u https://github.com/SuperiorOS/manifest -b thirteen -m crowdin.xml --depth=1
+          repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+          wait
+          cd crowdin
+          export CROWDIN_BASE_PATH=/home/runner/work/crowdin/crowdin/superior
+          CROWDIN_API_KEY=$CROWDIN_API_KEY
+          ./crowdin_sync.py --username Darkstar085 --branch thirteen --upload-sources --upload-translations
