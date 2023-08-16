e183e68ef4228c1560813b0188f069ef3188f962
S I P U N <sipunkumar85@gmail.com>
2023-08-17 03:16:49 +0530

    Update setup-crowdin.sh

--- .github/scripts/setup-crowdin.sh
+++ .github/scripts/setup-crowdin.sh
@@ -4,6 +4,7 @@
             wait
           wget https://artifacts.crowdin.com/repo/deb/crowdin3.deb -O crowdin.deb
           sudo dpkg -i crowdin.deb
+          sudo apt-get install python3-git python3-yaml python3-lxml
           wait
           git clone https://github.com/SuperiorOS/crowdin.git crowdin
           cd crowdin
